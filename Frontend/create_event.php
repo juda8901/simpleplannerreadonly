5a0ba164773b4578d0087dd854c5c519b0659050
Colin O'Connell <cooc5321@colorado.edu>
2017-10-30 22:43:50 -0600

    Updated create_event files to include location

--- Frontend/create_event.php
+++ Frontend/create_event.php
@@ -17,11 +17,12 @@
 <h3>Create an Event</h3>
 <form enctype="multipart/form-data" action="create_event_handler.php">
       <p>EventName:&nbsp <input type="text" name="Event Name" size="10" maxlength="20" /></p>
-	  <p>Host:&nbsp <input type="text" name="Host" size="10" maxlength="20" /></p>
 	  <p>Time:&nbsp <input type="text" name="Time" size="10" maxlength="20" /></p>
+	  <p>Location:&nbsp <input type="text" name="Location" size="10" maxlength="20" /></p>
+	  <p>Host:&nbsp <input type="text" name="Host" size="10" maxlength="20" /></p>
 	  <p>Description:&nbsp <input type="text" name="Description" size="10" maxlength="200" /></p>
       <p>Attendees:&nbsp <input type="text" name="Attendees" size="10" maxlength="20" /></p>
-	  <!-- Should add more fields - location, maybe specifically time start and time end, maybe different column for main host (the current user) and
+	  <!-- Should add more fields - maybe specifically time start and time end, maybe different column for main host (the current user) and
 	   cohosts, tags, etc. -->
 	  <br>
       <input type="submit" value="Create Event" /> &nbsp <input type="Reset" />
--- Frontend/create_event_handler.php
+++ Frontend/create_event_handler.php
@@ -8,12 +8,13 @@ if(mysqli_connect_errno()){
 //Get entries to form from create_event.php
 $EventName = $_REQUEST['EventName'];
 $Time = $_REQUEST['Time'];
+$Location = $_REQUEST['Location'];
 $Host = $_REQUEST['Host'];
 $Description = $_REQUEST['Description'];
 $Attendees = $_REQUEST['Attendees'];
 
 //Insert the values into the database
-$query = "INSERT INTO events VALUES ( ((select max(id) from events)+1),'$EventName','$Time','$Host','$Description','$Attendees');";
+$query = "INSERT INTO events (id,name,event_time,location,host,description,attendees) VALUES ( ((select max(id) from events)+1),'$EventName','$Time','$Location','$Host','$Description','$Attendees');";
 $resultset = mysqli_query($connection,$query);
 
 echo "Successfully created event!";
