12c8cf159cc9cfe5ab60b34b0e562e0ed9b5ca4c
juda8901 <juda8901@colorado.edu>
2017-11-06 20:35:35 -0700

    Created php script
    Created php script. Unable to test so don't know if it's working yet

--- Backend/create_account_handler.php
+++ Backend/create_account_handler.php
@@ -1,3 +1,17 @@
 <?php
+$name=$_REQUEST['name'];
+$email=$_REQUEST['email'];
+$password=$_REQUEST['password'];
+$conn=@mysqli_connect(localhost, root, JustClay, simple_planner);
+if(mysqli_connect_errno()){
+echo "<h4>Failed to connect to MySQL: </h4>".mysqli_connect_error();
+}
 
+$insert="INSERT INTO accounts (name,email,password) VALUES ($name,$email,$password);";
+if($conn->query($insert)==TRUE){
+	echo "Account created successfully";
+} else {
+	echo "Error: ".$insert."<br>".$conn->error;
+}
+$conn->close();
 ?>
