344d328d7bed6b9c48837a7c49a6138fcaf19e53
juda9801 <juda8901@colorado.edu>
2017-11-07 18:15:11 -0700

    Almost have create account working
    Currently puts empty strings into the database...

--- create_account_handler.php
+++ create_account_handler.php
@@ -1,13 +1,18 @@
 <?php
-$name=$_REQUEST['name'];
-$email=$_REQUEST['email'];
-$password=$_REQUEST['password'];
-$conn=@mysqli_connect(localhost, root, JustClay, simple_planner);
+$name=$_REQUEST['Name'];
+$email=$_REQUEST['Email'];
+$pass=$_REQUEST['Password'];
+
+$hostname='sql3.freemysqlhosting.net';
+$username='sql3203668';
+$password='663hN84yLR';
+$db='sql3203668';
+$conn=@mysqli_connect($host,$username,$password,$db);
 if(mysqli_connect_errno()){
 echo "<h4>Failed to connect to MySQL: </h4>".mysqli_connect_error();
 }
 
-$insert="INSERT INTO accounts (name,email,password) VALUES ($name,$email,$password);";
+$insert="INSERT INTO accounts (name,email,password) VALUES ('$name','$email','$pass');";
 if($conn->query($insert)==TRUE){
 	echo "Account created successfully";
 } else {
