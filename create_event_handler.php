fa33167620a74619bb4e8015e91d92f7cd578f15
Colin O'Connell <cooc5321@colorado.edu>
2017-11-07 18:05:21 -0700

    Added modal in main html for creating event, deleted create_event.php

--- create_event_handler.php
+++ create_event_handler.php
@@ -1,12 +1,12 @@
 <?php
 //Connect to the database
-$connection = @mysqli_connect (localhost,root,???,simple_planner);
+$connection = @mysqli_connect (localhost,root,arbhcojdmn,simple_planner);
 if(mysqli_connect_errno()){
 		echo "<h4>Failed to connect to MySQL:</h4>".mysqli_connect_error();
 	}
 	
-//Get entries to form from create_event.php
-$EventName = $_REQUEST['EventName'];
+//Get entries to form from simpleplannerv2.html
+$EventName = $_REQUEST['Event Name'];
 $Time = $_REQUEST['Time'];
 $Location = $_REQUEST['Location'];
 $Host = $_REQUEST['Host'];
@@ -18,5 +18,5 @@ $query = "INSERT INTO events (id,name,event_time,location,host,description,atten
 $resultset = mysqli_query($connection,$query);
 
 echo "Successfully created event!";
-include 'create_event.php';
+include 'simpleplannerv2.html';
 ?>
