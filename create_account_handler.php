b5fd51ac3b633144323c0a554b2764880cd2250b
juda8901 <juda8901@colorado.edu>
2017-11-08 19:33:56 -0700

    Functional
    Still should add something to make sure email is not already in use, make sure if password requirements no satisfied that can't create account, and add RSA

--- create_account_handler.php
+++ create_account_handler.php
@@ -1,22 +1,23 @@
 <?php
-$name=$_REQUEST['Name'];
-$email=$_REQUEST['Email'];
-$pass=$_REQUEST['Password'];
+	$name=$_REQUEST['Name'];
+	$email=$_REQUEST['Email'];
+	$pass=$_REQUEST['Password'];
 
-$hostname='sql3.freemysqlhosting.net';
-$username='sql3203668';
-$password='663hN84yLR';
-$db='sql3203668';
-$conn=@mysqli_connect($host,$username,$password,$db);
-if(mysqli_connect_errno()){
-echo "<h4>Failed to connect to MySQL: </h4>".mysqli_connect_error();
-}
+	$hostname='sql3.freemysqlhosting.net';
+	$username='sql3203668';
+	$password='663hN84yLR';
+	$db='sql3203668';
+	$conn=@mysqli_connect($hostname,$username,$password,$db);
+	if(mysqli_connect_errno()){
+		echo "<h4>Failed to connect to MySQL: </h4>".mysqli_connect_error();
+	}
 
-$insert="INSERT INTO accounts (name,email,password) VALUES ('$name','$email','$pass');";
-if($conn->query($insert)==TRUE){
-	echo "Account created successfully";
-} else {
-	echo "Error: ".$insert."<br>".$conn->error;
-}
-$conn->close();
+	$insert="INSERT INTO accounts (account_name,account_email,account_password) VALUES ('$name','$email','$pass');";
+	if($conn->query($insert)==TRUE){
+		echo "Account created successfully";
+		include 'simpleplannerv2.php';
+	} else {
+		echo "Error: ".$insert."<br>".$conn->error;
+	}
+	$conn->close();
 ?>
