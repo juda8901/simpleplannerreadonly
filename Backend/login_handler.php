b7f0ff163eaaf06ff01ffba51359cbf19fe9a5de
juda9801 <juda8901@colorado.edu>
2017-12-01 00:02:52 -0700

    Added files/updated existing to be able to host on Heroku

--- Backend/login_handler.php
+++ Backend/login_handler.php
@@ -1,7 +1,6 @@
 <?php
-	$name=$_REQUEST['Name'];
-	$email=$_REQUEST['Email'];
-	$pass=$_REQUEST['Password'];
+	$uname=$_REQUEST['uname'];
+	$psw=$_REQUEST['psw'];
 
 	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 	$server = $url["host"];
@@ -13,12 +12,12 @@
 		echo "<h4>Failed to connect to MySQL: </h4>".mysqli_connect_error();
 	}
 
-	$insert="INSERT INTO accounts (account_name,account_email,account_password) VALUES ('$name','$email','$pass');";
-	if($conn->query($insert)==TRUE){
-		echo "Account created successfully";
-		include_once '/Frontend/simpleplannerv2.php';
+	$query="SELECT * FROM accounts WHERE account_email='$uname' AND account_password='$psw';";
+	if($conn->query($query)==TRUE){
+		echo "Account found";
+		include_once '/Frontend/simpleplannerv2';
 	} else {
-		echo "Error: ".$insert."<br>".$conn->error;
+		echo "Error: ".$query."<br>".$conn->error;
 	}
 	$conn->close();
 ?>
