15f0c08a25906d7a7a8abffac580d0c55a51a7a3
Colin O'Connell <cooc5321@colorado.edu>
2017-12-05 18:10:55 -0700

    update event_is_hidden once more again

--- Backend/create_event_handler.php
+++ Backend/create_event_handler.php
@@ -24,7 +24,7 @@
 		echo "<h4>Failed to connect to MySQL: </h4>".mysqli_connect_error();
 	}
 	
-	$insert="INSERT INTO events (event_title,event_start_date_time,event_end_date_time,event_location,event_description,event_is_hidden) VALUES ('$EventTitle',STR_TO_DATE($StartTime,'%m/%d/%Y %h:%i %p'),STR_TO_DATE($EndTime,'%m/%d/%Y %h:%i %p'),'$Location','$Description',$priv);";
+	$insert="INSERT INTO events (event_title,event_start_date_time,event_end_date_time,event_location,event_description,event_is_hidden) VALUES ('$EventTitle',STR_TO_DATE('$StartTime','%m/%d/%Y %h:%i %p'),STR_TO_DATE('$EndTime','%m/%d/%Y %h:%i %p'),'$Location','$Description',$priv);";
 	if($conn->query($insert)==TRUE){
 		echo "Event created successfully";
 		include_once '/Frontend/simpleplannerv2.php';
