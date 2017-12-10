f99d2192ccb8ae6207af5e0dc726da616ffa9793
Michael Nicolaou <mikenicolaou1994@gmail.com>
2017-12-10 16:22:02 -0700

    9

--- app/Frontend/home.php
+++ app/Frontend/home.php
@@ -216,7 +216,7 @@
 
 
 		<!-- Event Cards -->
-		<div class="w3-container" style="background-color: rgba(0,0,0,.87); width: 85%; margin: auto;">
+		<div class="w3-container" style="width: 85%; margin: auto;">
 			<div class='w3-row' style=' margin: auto;'>
 				<?php
 				//$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
