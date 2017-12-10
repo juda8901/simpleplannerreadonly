b6c489c3ee88bd849771f7ab31d1cf2a4973f46a
Colin O'Connell <cooc5321@colorado.edu>
2017-12-10 15:08:46 -0700

    date now entered as text

--- app/Backend/create_event_handler.php
+++ app/Backend/create_event_handler.php
@@ -1,13 +1,13 @@
 <?php
-//session_start();
+session_start();
 //Get entries to form from simpleplannerv2.php
-$EventTitle = $_POST['EventTitle'];
-$StartTime = $_POST['Start Time'];
-$EndTime = $_POST['End Time'];
-$Location = $_POST['Location'];
-//$Host = $_SESSION['user_id'];
-$Description = $_POST['Description'];
-$PrivPub = $_POST['PrivPub'];
+$EventTitle = $_REQUEST['EventTitle'];
+$StartTime = $_REQUEST['Start Time'];
+$EndTime = $_REQUEST['End Time'];
+$Location = $_REQUEST['Location'];
+$Host = $_SESSION['user_id'];
+$Description = $_REQUEST['Description'];
+$PrivPub = $_REQUEST['PrivPub'];
 if($PrivPub == '1'){
 	$priv = 1;
 }
@@ -39,7 +39,7 @@ if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
 }
 
-$insert="INSERT INTO events (event_title,event_start_date_time,event_end_date_time,event_location,event_description,event_is_hidden) VALUES ('$EventTitle',$start,$end,'$Location','$Description',$priv);";
+$insert="INSERT INTO events (event_host_account_id,event_title,event_start_date_time,event_end_date_time,event_location,event_description,event_is_hidden) VALUES ('$Host','$EventTitle',$start,$end,'$Location','$Description',$priv);";
 if($conn->query($insert)==TRUE){
 	echo "Event created successfully";
 	header('Location: https://simpleplanner.herokuapp.com');
