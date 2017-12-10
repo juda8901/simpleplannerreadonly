e3b31384629c46108232bfe8e4896cc667d8698e
juda8901 <juda8901@colorado.edu>
2017-12-10 16:29:30 -0700

    Login might redirect

--- app/Frontend/login.php
+++ app/Frontend/login.php
@@ -40,28 +40,7 @@
 -->
 	<br>
 	<br>
-	<center style="margin-top: 7.5%;">
-		<form action="login.php" method="post" style="margin: 20px 0px 20px 0px; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2); width: 20em; height: 25em;">
-			<h1 style="float:left; margin:10px 10px 10px 20px;"> Login </h1>
-			<br>
-			<br>
-			<div class="container">
-				<p style="color: red; display: none;" id="error">Incorrect username or password. If you don't have an account click <u><a onclick="document.getElementById('sign_up').style.display='block'">here</a></u>.</p>
 
-				<input type="text" placeholder="Email" name="uname" style="width: 100%; padding: 8px 20px; margin: 8px 0; display: inline-block; border: 1px solid #ccc; box-sizing: border-box; border-radius: 12px;" required>
-
-				<input type="password" placeholder="Password" name="psw" style="width: 100%; padding: 8px 20px; margin: 8px 0; display: inline-block; border: 1px solid #ccc; box-sizing: border-box; border-radius: 12px;" required>
-
-				<input type="submit" class="w3-center w3-btn w3-xlarge w3-hover-white w3-blue-grey" value="Submit" style="color: #f13a59; margin: 20px 20px 20px 20px; font-weight:650;"/>
-
-				<div style="display: none;"><input id="remember" type="checkbox" checked="checked" style="float: left;"><span><a onclick="document.getElementById('remember').checked=!document.getElementById('remember').checked" class="w3-left w3-hover-light-grey"> Remember me </a></span></div>
-				<br>
-				<br>
-				<br>
-				<span class="psw" style="display: none;"><a href="#">Forgot password?</a></span>
-			</div>
-		</form>
-	</center>
 
 	<?php
 	$uname=$_POST['uname'];
@@ -94,6 +73,29 @@
 		$conn->close();
 	}
 	?>
+
+	<center style="margin-top: 7.5%;">
+		<form action="login.php" method="post" style="margin: 20px 0px 20px 0px; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2); width: 20em; height: 25em;">
+			<h1 style="float:left; margin:10px 10px 10px 20px;"> Login </h1>
+			<br>
+			<br>
+			<div class="container">
+				<p style="color: red; display: none;" id="error">Incorrect username or password. If you don't have an account click <u><a onclick="document.getElementById('sign_up').style.display='block'">here</a></u>.</p>
+
+				<input type="text" placeholder="Email" name="uname" style="width: 100%; padding: 8px 20px; margin: 8px 0; display: inline-block; border: 1px solid #ccc; box-sizing: border-box; border-radius: 12px;" required>
+
+				<input type="password" placeholder="Password" name="psw" style="width: 100%; padding: 8px 20px; margin: 8px 0; display: inline-block; border: 1px solid #ccc; box-sizing: border-box; border-radius: 12px;" required>
+
+				<input type="submit" class="w3-center w3-btn w3-xlarge w3-hover-white w3-blue-grey" value="Submit" style="color: #f13a59; margin: 20px 20px 20px 20px; font-weight:650;"/>
+
+				<div style="display: none;"><input id="remember" type="checkbox" checked="checked" style="float: left;"><span><a onclick="document.getElementById('remember').checked=!document.getElementById('remember').checked" class="w3-left w3-hover-light-grey"> Remember me </a></span></div>
+				<br>
+				<br>
+				<br>
+				<span class="psw" style="display: none;"><a href="#">Forgot password?</a></span>
+			</div>
+		</form>
+	</center>
 	<br>
 
 
