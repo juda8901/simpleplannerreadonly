363304f519643752aa05d2aa8def36a2fa56de28
juda8901 <juda8901@colorado.edu>
2017-12-10 17:20:18 -0700

    still lost

--- app/Frontend/login.php
+++ app/Frontend/login.php
@@ -1,4 +1,4 @@
-<?php require '/Backend/login_handler.php'; ?>
+<?php session_start(); ?>
 
 <html>
 <head>
@@ -38,7 +38,37 @@
 	<br>
 	<br>
 
-	<?php //require '/Backend/login_handler.php'; ?>
+
+	<?php
+	$uname=$_POST['uname'];
+	$psw=$_POST['psw'];
+
+	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+	$server = $url["host"];
+	$username = $url["user"];
+	$password = $url["pass"];
+	$db = substr($url["path"], 1);
+	$conn = new mysqli($server, $username, $password, $db);
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
+
+	$query="SELECT * FROM accounts WHERE account_email='$uname' AND account_password='$psw';";
+	$result=$conn->query($query);
+	if(!$result){
+		echo "<p>Error: ".$query."<br>".$conn->error."</p>";
+	} elseif ($result->num_rows <= 0) {
+		echo "<script>document.getElementById('error').style.display='block';</script>";
+	} else {
+		while($row=$result->fetch_assoc()) {
+			$_SESSION['user_id']=(int)$row['account_id'];
+			echo "<p>Session User ID: ".$_SESSION['user_id']."</p>";
+		}
+		header('Location: https://simpleplanner.herokuapp.com');
+	}
+	$conn->close();
+	?>
+
 	<center style="margin-top: 7.5%;">
 		<form action="login.php" method="post" style="margin: 20px 0px 20px 0px; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2); width: 20em; height: 25em;">
 			<h1 style="float:left; margin:10px 10px 10px 20px;"> Login </h1>
