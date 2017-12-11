0f431301f56e009a9d1289d78b617cd8e2ce8d60
Colin O'Connell <cooc5321@colorado.edu>
2017-12-11 00:19:31 -0700

    create_event only uses text and no date picker now, added tags and others into insert query

--- app/Backend/create_event_handler.php
+++ app/Backend/create_event_handler.php
@@ -1,33 +1,21 @@
 <?php
 session_start();
 $EventTitle = $_REQUEST['EventTitle'];
-$StartTime = $_REQUEST['Start Time'];
-$EndTime = $_REQUEST['End Time'];
+$StartDate = $_REQUEST['StartDate'];
+$StartTime = $_REQUEST['StartTime'];
+$EndDate = $_REQUEST['EndDate'];
+$EndTime = $_REQUEST['EndTime'];
 $Location = $_REQUEST['Location'];
 $Host = $_SESSION['id'];
 $Description = $_REQUEST['Description'];
 $PrivPub = $_REQUEST['PrivPub'];
+$Tags = $_REQUEST['Tags'];
 if($PrivPub == '1'){
 	$priv = 1;
 }
 else{
 	$priv = 0;
 }
-//$start = date(Y-m-d H:i:s,$StartTime);
-//$end = date(Y-m-d H:i:s,$EndTime);
-//$format = "%m/%d/%Y %h:%i %p"
-//$format = "%Y-%m-%d %h:%i:%s"
-//$start = strftime($format,$StartTime);
-//$end = strftime($format,$EndTime);
-//$startformat = date_create_from_format("m/d/Y h:i p",$StartTime)
-//$start = date_format($startformat,"Y-m-d H:i:s")
-//$endformat = date_create_from_format("m/d/Y h:i p",$EndTime)
-//$end = date_format($endformat,"Y-m-d H:i:s")
-//$start = date("Y-m-d H:i:s",strftime("%Y-%m-%d %H:%i:%s",$StartTime)); //strtotime($StartTime)
-//$end = date("Y-m-d H:i:s",strftime("%Y-%m-%d %H:%i:%s",$EndTime));
-//$start = date("Y-m-d H:i:s",strptime(strftime($format,$StartTime),$format));
-//$end = date("Y-m-d H:i:s",strptime(strftime($format,$EndTime),$format));
-
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 $server = $url["host"];
 $username = $url["user"];
@@ -38,7 +26,7 @@ if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
 }
 
-$insert="INSERT INTO events (event_host_account_id,event_title,event_start_date_time,event_end_date_time,event_location,event_description,event_is_hidden) VALUES ('$Host','$EventTitle','$StartTime','$EndTime','$Location','$Description',$priv);";
+$insert="INSERT INTO events (event_host_account_id,event_title,event_start_date_time,event_end_date_time,event_location,event_description,event_is_hidden,event_start_date,event_end_date,event_tags) VALUES ('$Host','$EventTitle','$StartTime','$EndTime','$Location','$Description',$priv,'$StartDate','$EndDate','$Tags');";
 if($conn->query($insert)==TRUE){
 	echo "Event created successfully";
 	header('Location: https://simpleplanner.herokuapp.com');
