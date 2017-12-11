640ae14b452658999c70c5015f1380576e9dfaef
juda9801 <juda8901@colorado.edu>
2017-12-10 20:17:25 -0700

    Moved scripts to bottom of file

--- app/Frontend/home.php
+++ app/Frontend/home.php
@@ -62,80 +62,6 @@ $conn->close();
 			</a>
 		</h2>
 	</header>
-
-	<!-- Scripts for Header -->
-	<script type="text/javascript">
-	// Rotating text
-	(function() {
-		var quotes = $(".quotes");
-		var quoteIndex = -1;
-
-		function showNextQuote() {
-			++quoteIndex;
-			quotes.eq(quoteIndex % quotes.length)
-			.fadeIn(2000)
-			.delay(2000)
-			.fadeOut(2000, showNextQuote);
-		}
-
-		showNextQuote();
-	})();
-
-	var TxtType = function(el, toRotate, period) {
-		this.toRotate = toRotate;
-		this.el = el;
-		this.loopNum = 0;
-		this.period = parseInt(period, 10) || 3000;
-		this.txt = '';
-		this.tick();
-		this.isDeleting = false;
-	};
-
-	TxtType.prototype.tick = function() {
-		var i = this.loopNum % this.toRotate.length;
-		var fullTxt = this.toRotate[i];
-		if (this.isDeleting) {
-			this.txt = fullTxt.substring(0, this.txt.length - 1);
-		} else {
-			this.txt = fullTxt.substring(0, this.txt.length + 1);
-		}
-
-		this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-		var that = this;
-		var delta = 200 - Math.random() * 100;
-
-		if (this.isDeleting) { delta /= 2; }
-
-		if (!this.isDeleting && this.txt === fullTxt) {
-			delta = this.period;
-			this.isDeleting = true;
-		} else if (this.isDeleting && this.txt === '') {
-			this.isDeleting = false;
-			this.loopNum++;
-			delta = 500;
-		}
-
-		setTimeout(function() {
-			that.tick();
-		}, delta);
-	};
-
-	window.onload = function() {
-		var elements = document.getElementsByClassName('typewrite');
-		for (var i=0; i<elements.length; i++) {
-			var toRotate = elements[i].getAttribute('data-type');
-			var period = elements[i].getAttribute('data-period');
-			if (toRotate) {
-				new TxtType(elements[i], JSON.parse(toRotate), period);
-			}
-		}
-		// INJECT CSS
-		var css = document.createElement("style");
-		css.type = "text/css";
-		css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
-		document.body.appendChild(css);
-	};
-	</script>
 	<hr>
 
 	<!-- search bar -->
@@ -146,157 +72,56 @@ $conn->close();
 	<form>
 		<input type="text" name="search" placeholder="Search..">
 	</form>
-
-	<!-- search script -->
-	<script type="text/javascript">
-	$('.w3-row').hide();
-	$('#search').click(function(){
-		$('.w3-row').hide();
-		var s = $('#search-criteria').val();
-		$('.w3-row').each(function(){
-			if($(this).text().toUpperCase().indexOf(s.toUpperCase()) != -1){
-				$(this).show();
-			}
-		});
-	});
-	</script>
 	<hr>
 
 	<!-- Create Event Button -->
-	<button class="w3-btn w3-round-xxlarge w3-xlarge w3-hover-light-grey w3-blue-grey" onclick="<?php if($logged_in){	echo "document.getElementById('create_event').style.display='block'";} else {	echo "alert('You must log in first');window.location = 'login.php';";} ?>" style="margin: 15px; padding-left: 20px; padding-right: 25px;">+ Create Event</button>
+	<button class="w3-btn w3-round-xxlarge w3-xlarge w3-hover-light-grey w3-blue-grey" onclick="<?php if($logged_in){	echo "document.getElementById('create_event').style.display='block'";} else {	echo "alert('You must log in first');window.location = 'https://simpleplanner.herokuapp.com/Frontend/login.php';";} ?>" style="margin: 15px; padding-left: 20px; padding-right: 25px;">+ Create Event</button>
 
 
-	<!-- Modal for Create Event -->
-	<div id="create_event" class="w3-modal">
-		<div class="w3-modal-content w3-card-1 w3-animate-top">
-			<header class="w3-container w3-card w3-round w3-theme-l1">
-				<span onclick="document.getElementById('create_event').style.display='none'"
-				class="w3-button w3-display-topright">×</span>
-				<div class="w3-center">
-					<form class="w3-center w3-container w3-card-1" enctype="multipart/form-data" action="/Backend/create_event_handler.php">
-						<h2>Create an Event!</h2>
-						<div class="w3-section">
-							<input class="w3-input" name="EventTitle" type="text" required>
-							<label>Event Title</label>
-						</div>
-						<div class="w3-section">
-							<select class="w3-input" name="PrivPub">
-								<option value="1">Private</option>
-								<option value="0">Public</option>
-							</select>
-						</div>
-						<div id="datepick">
-							<input class="date start" name="StartDate" type="text" required>
-							<input class="time start" name="StartTime" type="text" required>
-							<input class="date end" name="EndDate" type="text" required>
-							<input class="time end" name="EndTime" type="text" required>
-						</div>
+		<!-- Modal for Create Event -->
+		<div id="create_event" class="w3-modal">
+			<div class="w3-modal-content w3-card-1 w3-animate-top">
+				<header class="w3-container w3-card w3-round w3-theme-l1">
+					<span onclick="document.getElementById('create_event').style.display='none'"
+					class="w3-button w3-display-topright">×</span>
+					<div class="w3-center">
+						<form class="w3-center w3-container w3-card-1" enctype="multipart/form-data" action="/Backend/create_event_handler.php">
+							<h2>Create an Event!</h2>
+							<div class="w3-section">
+								<input class="w3-input" name="EventTitle" type="text" required>
+								<label>Event Title</label>
+							</div>
+							<div class="w3-section">
+								<select class="w3-input" name="PrivPub">
+									<option value="1">Private</option>
+									<option value="0">Public</option>
+								</select>
+							</div>
+							<div id="datepick">
+								<input class="date start" name="StartDate" type="text" required>
+								<input class="time start" name="StartTime" type="text" required>
+								<input class="date end" name="EndDate" type="text" required>
+								<input class="time end" name="EndTime" type="text" required>
+							</div>
 
-						<div class="w3-section" id="locationField">
-							<input class="w3-input" name="Location" id="autocomplete" placeholder="Enter the address" onFocus="geolocate()" type="text" required>
-							<label>Location </label>
-						</div>
-						<div class="w3-section">
-							<input class="w3-input" name="Description" type="text" required>
-							<label>Description </label>
-						</div>
-						<div class="w3-section">
-							<input class="w3-input" name="Tags" type="text" required>
-							<label>Tags </label>
-						</div>
-						<input class="w3-center w3-btn w3-xlarge w3-hover-light-grey" style="color: #f13a59; margin: 20px 20px 20px 20px; width:40%; font-weight:650;" type="submit" value="Create Event" />
-					</form>
-				</div>
-			</header>
+							<div class="w3-section" id="locationField">
+								<input class="w3-input" name="Location" id="autocomplete" placeholder="Enter the address" onFocus="geolocate()" type="text" required>
+								<label>Location </label>
+							</div>
+							<div class="w3-section">
+								<input class="w3-input" name="Description" type="text" required>
+								<label>Description </label>
+							</div>
+							<div class="w3-section">
+								<input class="w3-input" name="Tags" type="text" required>
+								<label>Tags </label>
+							</div>
+							<input class="w3-center w3-btn w3-xlarge w3-hover-light-grey" style="color: #f13a59; margin: 20px 20px 20px 20px; width:40%; font-weight:650;" type="submit" value="Create Event" />
+						</form>
+					</div>
+				</header>
+			</div>
 		</div>
-	</div>
-
-	<link rel="stylesheet" type="text/css" href="jquery.timepicker.css" />
-	<link rel="stylesheet" type="text/css" href="bootstrap-datepicker.css" />
-	<script type="text/javascript" src="bootstrap-datepicker.js"></script>
-	<script type="text/javascript" src="jquery.timepicker.js"></script>
-	<script type="text/javascript" src="datepair.js"></script>
-	<script>
-	// initialize input widgets first
-	$('#datepick .time').timepicker({
-		'showDuration': true,
-		'timeFormat': 'g:ia'
-	});
-
-	$('#datepick .date').datepicker({
-		'format': 'm/d/yyyy',
-		'autoclose': true
-	});
-
-	// initialize datepair
-	var datepickEl = document.getElementById('datepick');
-	var datepair = new Datepair(datepickEl);
-	</script>
-
-	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCLsFEUG5AKf3-PEgQryg5RxPsQdD89dsI&libraries=places&callback=initAutocomplete"
-	async defer></script>
-	<script>
-	var placeSearch, autocomplete;
-	var componentForm = {
-		street_number: 'short_name',
-		route: 'long_name',
-		locality: 'long_name',
-		administrative_area_level_1: 'short_name',
-		country: 'long_name',
-		postal_code: 'short_name'
-	};
-
-	function initAutocomplete() {
-		// Create the autocomplete object, restricting the search to geographical
-		// location types.
-		autocomplete = new google.maps.places.Autocomplete(
-			/** @type {!HTMLInputElement} */(document.getElementById('autocomplete')),
-			{types: ['geocode']});
-
-			// When the user selects an address from the dropdown, populate the address
-			// fields in the form.
-			autocomplete.addListener('place_changed', fillInAddress);
-		}
-
-		function fillInAddress() {
-			// Get the place details from the autocomplete object.
-			var place = autocomplete.getPlace();
-
-			for (var component in componentForm) {
-				document.getElementById(component).value = '';
-				document.getElementById(component).disabled = false;
-			}
-
-			// Get each component of the address from the place details
-			// and fill the corresponding field on the form.
-			for (var i = 0; i < place.address_components.length; i++) {
-				var addressType = place.address_components[i].types[0];
-				if (componentForm[addressType]) {
-					var val = place.address_components[i][componentForm[addressType]];
-					document.getElementById(addressType).value = val;
-				}
-			}
-		}
-
-		// Bias the autocomplete object to the user's geographical location,
-		// as supplied by the browser's 'navigator.geolocation' object.
-		function geolocate() {
-			if (navigator.geolocation) {
-				navigator.geolocation.getCurrentPosition(function(position) {
-					var geolocation = {
-						lat: position.coords.latitude,
-						lng: position.coords.longitude
-					};
-					var circle = new google.maps.Circle({
-						center: geolocation,
-						radius: position.coords.accuracy
-					});
-					autocomplete.setBounds(circle.getBounds());
-				});
-			}
-		}
-		//from Google: https://developers.google.com/maps/documentation/javascript/examples/places-autocomplete-addressform
-		</script>
 
 
 		<!-- Event Cards -->
@@ -370,24 +195,207 @@ $conn->close();
 			<h1>Events Happening Nearby</h1>
 			<div id="map"></div>
 			<hr>
-			<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCDKE8pn4aOs2nsQ8pkn9vxxLJQu6KYI90&callback=initMap"></script>
-			<script>
-			function initMap() {
-				var Boulder = {lat: 40.027443, lng: -105.25174};
-				var map = new google.maps.Map(document.getElementById('map'), {
-					zoom: 4,
-					center: Boulder
-				});
-				var marker = new google.maps.Marker({
-					position: Boulder,
-					map: map
-				});
-			}
-			</script>
 			<hr>
 
 
 			<!-- Footer -->
 			<?php require 'footer.html'; ?>
-		</body>
-		</html>
+
+
+			<!-- ############################################## SCRIPTS ######################################################## -->
+			<!-- Scripts for Header -->
+			<script type="text/javascript">
+			// Rotating text
+			(function() {
+				var quotes = $(".quotes");
+				var quoteIndex = -1;
+
+				function showNextQuote() {
+					++quoteIndex;
+					quotes.eq(quoteIndex % quotes.length)
+					.fadeIn(2000)
+					.delay(2000)
+					.fadeOut(2000, showNextQuote);
+				}
+
+				showNextQuote();
+			})();
+
+			var TxtType = function(el, toRotate, period) {
+				this.toRotate = toRotate;
+				this.el = el;
+				this.loopNum = 0;
+				this.period = parseInt(period, 10) || 3000;
+				this.txt = '';
+				this.tick();
+				this.isDeleting = false;
+			};
+
+			TxtType.prototype.tick = function() {
+				var i = this.loopNum % this.toRotate.length;
+				var fullTxt = this.toRotate[i];
+				if (this.isDeleting) {
+					this.txt = fullTxt.substring(0, this.txt.length - 1);
+				} else {
+					this.txt = fullTxt.substring(0, this.txt.length + 1);
+				}
+
+				this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+				var that = this;
+				var delta = 200 - Math.random() * 100;
+
+				if (this.isDeleting) { delta /= 2; }
+
+				if (!this.isDeleting && this.txt === fullTxt) {
+					delta = this.period;
+					this.isDeleting = true;
+				} else if (this.isDeleting && this.txt === '') {
+					this.isDeleting = false;
+					this.loopNum++;
+					delta = 500;
+				}
+
+				setTimeout(function() {
+					that.tick();
+				}, delta);
+			};
+
+			window.onload = function() {
+				var elements = document.getElementsByClassName('typewrite');
+				for (var i=0; i<elements.length; i++) {
+					var toRotate = elements[i].getAttribute('data-type');
+					var period = elements[i].getAttribute('data-period');
+					if (toRotate) {
+						new TxtType(elements[i], JSON.parse(toRotate), period);
+					}
+				}
+				// INJECT CSS
+				var css = document.createElement("style");
+				css.type = "text/css";
+				css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+				document.body.appendChild(css);
+			};
+		</script>
+
+
+		<!-- search script -->
+		<script type="text/javascript">
+		$('.w3-row').hide();
+		$('#search').click(function(){
+			$('.w3-row').hide();
+			var s = $('#search-criteria').val();
+			$('.w3-row').each(function(){
+				if($(this).text().toUpperCase().indexOf(s.toUpperCase()) != -1){
+					$(this).show();
+				}
+			});
+		});
+	</script>
+
+
+	<!-- Scripts for Create Event -->
+	<link rel="stylesheet" type="text/css" href="jquery.timepicker.css" />
+	<link rel="stylesheet" type="text/css" href="bootstrap-datepicker.css" />
+	<script type="text/javascript" src="bootstrap-datepicker.js"></script>
+	<script type="text/javascript" src="jquery.timepicker.js"></script>
+	<script type="text/javascript" src="datepair.js"></script>
+	<script>
+	// initialize input widgets first
+	$('#datepick .time').timepicker({
+		'showDuration': true,
+		'timeFormat': 'g:ia'
+	});
+
+	$('#datepick .date').datepicker({
+		'format': 'm/d/yyyy',
+		'autoclose': true
+	});
+
+	// initialize datepair
+	var datepickEl = document.getElementById('datepick');
+	var datepair = new Datepair(datepickEl);
+</script>
+
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCLsFEUG5AKf3-PEgQryg5RxPsQdD89dsI&libraries=places&callback=initAutocomplete"
+async defer></script>
+<script>
+var placeSearch, autocomplete;
+var componentForm = {
+	street_number: 'short_name',
+	route: 'long_name',
+	locality: 'long_name',
+	administrative_area_level_1: 'short_name',
+	country: 'long_name',
+	postal_code: 'short_name'
+};
+
+function initAutocomplete() {
+	// Create the autocomplete object, restricting the search to geographical
+	// location types.
+	autocomplete = new google.maps.places.Autocomplete(
+		/** @type {!HTMLInputElement} */(document.getElementById('autocomplete')),
+		{types: ['geocode']});
+
+		// When the user selects an address from the dropdown, populate the address
+		// fields in the form.
+		autocomplete.addListener('place_changed', fillInAddress);
+	}
+
+	function fillInAddress() {
+		// Get the place details from the autocomplete object.
+		var place = autocomplete.getPlace();
+
+		for (var component in componentForm) {
+			document.getElementById(component).value = '';
+			document.getElementById(component).disabled = false;
+		}
+
+		// Get each component of the address from the place details
+		// and fill the corresponding field on the form.
+		for (var i = 0; i < place.address_components.length; i++) {
+			var addressType = place.address_components[i].types[0];
+			if (componentForm[addressType]) {
+				var val = place.address_components[i][componentForm[addressType]];
+				document.getElementById(addressType).value = val;
+			}
+		}
+	}
+
+	// Bias the autocomplete object to the user's geographical location,
+	// as supplied by the browser's 'navigator.geolocation' object.
+	function geolocate() {
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(function(position) {
+				var geolocation = {
+					lat: position.coords.latitude,
+					lng: position.coords.longitude
+				};
+				var circle = new google.maps.Circle({
+					center: geolocation,
+					radius: position.coords.accuracy
+				});
+				autocomplete.setBounds(circle.getBounds());
+			});
+		}
+	}
+	//from Google: https://developers.google.com/maps/documentation/javascript/examples/places-autocomplete-addressform
+</script>
+
+
+<!-- Scripts for Google Map -->
+<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCDKE8pn4aOs2nsQ8pkn9vxxLJQu6KYI90&callback=initMap"></script>
+<script>
+function initMap() {
+	var Boulder = {lat: 40.027443, lng: -105.25174};
+	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 4,
+		center: Boulder
+	});
+	var marker = new google.maps.Marker({
+		position: Boulder,
+		map: map
+	});
+}
+</script>
+</body>
+</html>
