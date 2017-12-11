a6fd03c73561ea8ce42f5701aa062c10e17c9c76
juda8901 <juda8901@colorado.edu>
2017-12-10 17:05:09 -0700

    Trying to redirect

--- app/Frontend/login.php
+++ app/Frontend/login.php
@@ -40,7 +40,7 @@
 
 
 	<center style="margin-top: 7.5%;">
-		<form action="login2.php" method="post" style="margin: 20px 0px 20px 0px; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2); width: 20em; height: 25em;">
+		<form action="/Backend/login_handler.php" method="post" style="margin: 20px 0px 20px 0px; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2); width: 20em; height: 25em;">
 			<h1 style="float:left; margin:10px 10px 10px 20px;"> Login </h1>
 			<br>
 			<br>
