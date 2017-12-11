48bffd42d21ef53e0f53080057f27b0008bef824
juda8901 <juda8901@colorado.edu>
2017-12-10 17:18:31 -0700

    pls

--- app/Frontend/login.php
+++ app/Frontend/login.php
@@ -38,7 +38,7 @@
 	<br>
 	<br>
 
-	<?php require '/Backend/login_handler.php'; ?>
+	<?php //require '/Backend/login_handler.php'; ?>
 	<center style="margin-top: 7.5%;">
 		<form action="login.php" method="post" style="margin: 20px 0px 20px 0px; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2); width: 20em; height: 25em;">
 			<h1 style="float:left; margin:10px 10px 10px 20px;"> Login </h1>
