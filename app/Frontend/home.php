dc249bde4fc398bf5c7d08336a0df123d9e98622
Michael Nicolaou <mikenicolaou1994@gmail.com>
2017-12-12 05:54:21 -0700

    f

--- app/Frontend/home.php
+++ app/Frontend/home.php
@@ -42,15 +42,15 @@ if($valid) $id=$_SESSION['id'];
 	<div class="homepage-hero-module">
     <div class="video-container">
     	<div class="title-container" >
-    		<h3 > sdfsdfsdfsdfs </h3>
+        <h3 style="position:absolute; top:0; left:0; z-index:1;"> sdfsdfsdfsdfs </h3>
     	</div>
         <div class="filter"></div>
-        <h3 > sdfsdfsdfsdfs </h3>
+        <h3 style="position:absolute; top:0; left:0; z-index:1;"> sdfsdfsdfsdfs </h3>
         <video autoplay loop class="fillWidth">
-        	<h3 > sdfsdfsdfsdfs </h3>
+        <h3 style="position:absolute; top:0; left:0; z-index:1;"> sdfsdfsdfsdfs </h3>
             <source src="https://simpleplanner.herokuapp.com/Frontend/Up.mp4" type="video/mp4" />Your browser does not support the video tag. I suggest you upgrade your browser.
             <source src="https://simpleplanner.herokuapp.com/Frontend/Up.webm" type="video/webm" />Your browser does not support the video tag. I suggest you upgrade your browser.
-            <h3 > sdfsdfsdfsdfs </h3>
+        <h3 style="position:absolute; top:0; left:0; z-index:1;"> sdfsdfsdfsdfs </h3>
         </video>
         <!--<div class="poster hidden">
             <img src="https://simpleplanner.herokuapp.com/Frontend/Up.jpg" alt="">
