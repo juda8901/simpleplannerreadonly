1168ad851e396adb018fc6b8a53c403e132eb14e
Michael Nicolaou <mikenicolaou1994@gmail.com>
2017-12-12 02:18:06 -0700

    xx

--- app/Frontend/home.php
+++ app/Frontend/home.php
@@ -136,13 +136,13 @@ div.sticky {
 	<div style ="position: -webkit-sticky; position: sticky; top: 50px; background-color: grey;">
 	<form id="search_bar" style="width: 100%; text-align: center;" onsubmit="return false">
 		<input type="text" placeholder="  Search for an event..." name="search-criteria" id="search-criteria" style="width: 40%; border-radius: 30px; font-size: large; padding: 0; margin: 0;"/>
-		
+		<button type="submit" style="width: 30px; height: 30px; padding: 0; margin:0px; margin-bottom: 10px;" id="search" value="search"><img src="https://simpleplanner.herokuapp.com/Frontend/images/searchIconRed.png" style="width: 30px; height: 30px;"><i class="fa fa-search" style="zoom: 1.75;padding: 0; margin: 0;"></i></button>
 	</form>
 	</div>
 
 	
 
-			<!-- search script <button type="submit" style="width: 30px; height: 30px; padding: 0; margin:0px; margin-bottom: 10px;" id="search" value="search"><img src="https://simpleplanner.herokuapp.com/Frontend/images/searchIconRed.png" style="width: 30px; height: 30px;"><i class="fa fa-search" style="zoom: 1.75;padding: 0; margin: 0;"></i></button>-->
+			<!-- search script -->
 			<script type="text/javascript">
 			$('.w3-card').hide();
 			$('#search').click(function(){
