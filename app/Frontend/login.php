b2c7c6fc4d4be8f059da1818088c9ea428aea214
Ariel Riggan <ariel.riggan@colorado.edu>
2017-12-11 23:08:58 -0700

    tiny change

--- app/Frontend/login.php
+++ app/Frontend/login.php
@@ -30,7 +30,7 @@ if(isset($_POST['submit'])){
 	$conn->close();
 }
 if($_SESSION['logged_in']){
-	echo "<script type=text/javascript>window.location('https://simpleplanner.herokuapp.com/Frontend/accountTemplate.php')</script>";
+	echo "<script type=text/javascript>window.location('accountTemplate.php')</script>";
 	//header('Location: https://simpleplanner.herokuapp.com/Frontend/accountTemplate.php');
 	die();
 }
