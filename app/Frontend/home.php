2b5facc6a54809e291ee855ef80ecca9b68155fa
Michael Nicolaou <mikenicolaou1994@gmail.com>
2017-12-12 06:16:51 -0700

    f

--- app/Frontend/home.php
+++ app/Frontend/home.php
@@ -45,9 +45,9 @@ if($valid) $id=$_SESSION['id'];
     	</div>
         <div class="filter"></div>
         <header class="w3-theme" id="Header">
-		<h1 style="color: #F64060;">Simpleplanner</h1>
+		<h1 style="color: white;">Simpleplanner</h1>
 		<h2>
-			<a href="" class="typewrite" style="text-decoration: none; font-size: 25px; color:#2e3e48;" data-type='[ "Planning made simple.", "Change the world.", "Flawlessly connect with others." ]'>
+			<a href="" class="typewrite" style="text-decoration: none; font-size: 25px; color:#F64060;" data-type='[ "Planning made simple.", "Change the world.", "Flawlessly connect with others." ]'>
 				<span class="wrap"></span>
 			</a>
 		</h2>
