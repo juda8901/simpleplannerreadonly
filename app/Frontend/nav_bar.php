77dff798be5a3572e0881bc588ee2a57282bb0c0
Michael Nicolaou <mikenicolaou1994@gmail.com>
2017-12-11 19:36:32 -0700

    1

--- app/Frontend/nav_bar.php
+++ app/Frontend/nav_bar.php
@@ -1,7 +1,7 @@
 <div class="w3-bar w3-white w3-top" style= " box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.1), 0 6px 20px 0 rgba(0, 0, 0, 0.5);">
   <a href="https://simpleplanner.herokuapp.com" class="w3-bar-item w3-left w3-button "><img src="https://simpleplanner.herokuapp.com/Frontend/images/treeLogo.png" style="width: 30px; height: 30px;"></a>
 <div class="w3-bar w3-white w3-top">
-  <a href="https://simpleplanner.herokuapp.com" class="w3-bar-item w3-left w3-button w3-hover-blue-grey"><img src="https://simpleplanner.herokuapp.com/Frontend/images/treeLogo.png" style="width: 20px; height: 20px;"><span id="nav_title" style="display: none;">  Simpleplanner</span></a>
+  <a href="https://simpleplanner.herokuapp.com" class="w3-bar-item w3-left w3-button"><img src="https://simpleplanner.herokuapp.com/Frontend/images/treeLogo.png" style="width: 20px; height: 20px;"><span id="nav_title" style="display: none;">  Simpleplanner</span></a>
   <a href="Frontend/login.php" class="w3-bar-item w3-right w3-button w3-hover-white" id="login">Log In</a>
   <a href="https://simpleplanner.herokuapp.com/Backend/logout.php" class="w3-bar-item w3-right w3-button w3-hover-white" id="logout">Log Out</a>
   <a onclick="document.getElementById('sign_up').style.display='block'" class=" w3-bar-item w3-right w3-button w3-hover-white" id="reg">Sign Up</a>
