18a0c6226e000f979f5838b807bb25df4c85562d
Michael Nicolaou <mikenicolaou1994@gmail.com>
2017-12-12 07:34:53 -0700

    ddd

--- app/Frontend/home.php
+++ app/Frontend/home.php
@@ -257,7 +257,7 @@ if($valid) $id=$_SESSION['id'];
 			
 
 			<!-- Modal for Create Event -->
-			<div id="create_event" class="w3-modal">
+			<div id="create_event" class="w3-modal" style="margin:0;">
 				<div class="w3-modal-content w3-card-1 w3-animate-top">
 					<header class="w3-container w3-card w3-round w3-theme-l1">
 						<span onclick="document.getElementById('create_event').style.display='none'"
