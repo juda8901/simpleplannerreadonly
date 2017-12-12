6fb1a125efcf43a32b16366bccc36cddb702d89e
Michael Nicolaou <mikenicolaou1994@gmail.com>
2017-12-11 21:22:18 -0700

    123123

--- app/Frontend/home.php
+++ app/Frontend/home.php
@@ -44,19 +44,6 @@ width: 100%;
 				<span class="wrap"></span>
 			</a>
 		</h2>
-		<style>
-      /* Always set the map height explicitly to define the size of the div
-       * element that contains the map. */
-      #map {
-        height: 100%;
-      }
-      /* Optional: Makes the sample page fill the window. */
-      html, body {
-        height: 100%;
-        margin: 0;
-        padding: 0;
-      }
-    </style>
 	</header>
 
 	<!-- Scripts for Header -->
