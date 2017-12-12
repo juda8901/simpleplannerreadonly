00b0cee33101554c3fb25aaacb8ce164e320bda0
Michael Nicolaou <mikenicolaou1994@gmail.com>
2017-12-12 04:12:57 -0700

    dd

--- app/Frontend/home.php
+++ app/Frontend/home.php
@@ -45,9 +45,6 @@ if($valid) $id=$_SESSION['id'];
             <source src="https://simpleplanner.herokuapp.com/Frontend/Up.mp4" type="video/mp4" />Your browser does not support the video tag. I suggest you upgrade your browser.
             <source src="https://simpleplanner.herokuapp.com/Frontend/Up.webm" type="video/webm" />Your browser does not support the video tag. I suggest you upgrade your browser.
         </video>
-        <div class="poster hidden">
-            <img src="https://simpleplanner.herokuapp.com/Frontend/Up.jpg" alt="">
-        </div>
     </div>
 </div>
 
