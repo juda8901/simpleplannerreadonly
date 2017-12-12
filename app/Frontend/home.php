d819cd5e06714f9bb004be6f2da31533adde169d
Michael Nicolaou <mikenicolaou1994@gmail.com>
2017-12-12 05:05:56 -0700

    dd

--- app/Frontend/home.php
+++ app/Frontend/home.php
@@ -33,14 +33,95 @@ if($valid) $id=$_SESSION['id'];
 </style>
 </head>
 
-<!-- <div class="homepage-hero-module"> -->
+ 
+
+<body>
+	<!-- Navigation Bar -->
+	<?php require 'nav_bar.php'; ?>
+	<br><br>
+
+	<div class="homepage-hero-module">
     <div class="video-container">
-        <!--<div class="filter"></div> -->
+        <div class="filter"></div>
         <video autoplay loop class="fillWidth">
             <source src="https://simpleplanner.herokuapp.com/Frontend/Up.mp4" type="video/mp4" />Your browser does not support the video tag. I suggest you upgrade your browser.
             <source src="https://simpleplanner.herokuapp.com/Frontend/Up.webm" type="video/webm" />Your browser does not support the video tag. I suggest you upgrade your browser.
         </video>
-        <header class="w3-theme" id="Header">
+        <div class="poster hidden">
+            <img src="https://simpleplanner.herokuapp.com/Frontend/Up.png" alt="">
+        </div>
+        
+    </div>
+</div>
+
+<script >
+					$( document ).ready(function() {
+
+					    scaleVideoContainer();
+
+					    initBannerVideoSize('.video-container .poster img');
+					    initBannerVideoSize('.video-container .filter');
+					    initBannerVideoSize('.video-container video');
+
+					    $(window).on('resize', function() {
+					        scaleVideoContainer();
+					        scaleBannerVideoSize('.video-container .poster img');
+					        scaleBannerVideoSize('.video-container .filter');
+					        scaleBannerVideoSize('.video-container video');
+					    });
+
+					});
+
+					function scaleVideoContainer() {
+
+					    var height = $(window).height() + 5;
+					    var unitHeight = parseInt(height) + 'px';
+					    $('.homepage-hero-module').css('height',unitHeight);
+
+					}
+
+					function initBannerVideoSize(element){
+
+					    $(element).each(function(){
+					        $(this).data('height', $(this).height());
+					        $(this).data('width', $(this).width());
+					    });
+
+					    scaleBannerVideoSize(element);
+
+					}
+
+					function scaleBannerVideoSize(element){
+
+					    var windowWidth = $(window).width(),
+					    windowHeight = $(window).height() + 5,
+					    videoWidth,
+					    videoHeight;
+
+					    // console.log(windowHeight);
+
+					    $(element).each(function(){
+					        var videoAspectRatio = $(this).data('height')/$(this).data('width');
+
+					        $(this).width(windowWidth);
+
+					        if(windowWidth < 1000){
+					            videoHeight = windowHeight;
+					            videoWidth = videoHeight / videoAspectRatio;
+					            $(this).css({'margin-top' : 0, 'margin-left' : -(videoWidth - windowWidth) / 2 + 'px'});
+
+					            $(this).width(videoWidth).height(videoHeight);
+					        }
+
+					        $('.homepage-hero-module .video-container video').addClass('fadeIn animated');
+
+					    });
+					}
+					</script>
+
+
+	<!-- Header -->
+	<header class="w3-theme" id="Header">
 		<h1 style="color: #F64060;">Simpleplanner</h1>
 		<h2>
 			<a href="" class="typewrite" style="text-decoration: none; font-size: 25px; color:#2e3e48;" data-type='[ "Planning made simple.", "Change the world.", "Flawlessly connect with others." ]'>
@@ -49,18 +130,6 @@ if($valid) $id=$_SESSION['id'];
 		</h2>
 
 	</header>
-    </div>
-<!-- </div> -->
-
-<body>
-	<!-- Navigation Bar -->
-	<?php require 'nav_bar.php'; ?>
-	<br><br>
-
-
-
-
-	<!-- Header -->
 	
 
 	<!-- Scripts for Header -->
@@ -549,69 +618,6 @@ if($valid) $id=$_SESSION['id'];
 					<!-- Footer -->
 					<?php require 'footer.html'; ?>
 
-					<script >
-					$( document ).ready(function() {
-
-					    scaleVideoContainer();
-
-					    initBannerVideoSize('.video-container .poster img');
-					    initBannerVideoSize('.video-container .filter');
-					    initBannerVideoSize('.video-container video');
-
-					    $(window).on('resize', function() {
-					        scaleVideoContainer();
-					        scaleBannerVideoSize('.video-container .poster img');
-					        scaleBannerVideoSize('.video-container .filter');
-					        scaleBannerVideoSize('.video-container video');
-					    });
-
-					});
-
-					function scaleVideoContainer() {
-
-					    var height = $(window).height() + 5;
-					    var unitHeight = parseInt(height) + 'px';
-					    $('.homepage-hero-module').css('height',unitHeight);
-
-					}
-
-					function initBannerVideoSize(element){
-
-					    $(element).each(function(){
-					        $(this).data('height', $(this).height());
-					        $(this).data('width', $(this).width());
-					    });
-
-					    scaleBannerVideoSize(element);
-
-					}
-
-					function scaleBannerVideoSize(element){
-
-					    var windowWidth = $(window).width(),
-					    windowHeight = $(window).height() + 5,
-					    videoWidth,
-					    videoHeight;
-
-					    // console.log(windowHeight);
-
-					    $(element).each(function(){
-					        var videoAspectRatio = $(this).data('height')/$(this).data('width');
-
-					        $(this).width(windowWidth);
-
-					        if(windowWidth < 1000){
-					            videoHeight = windowHeight;
-					            videoWidth = videoHeight / videoAspectRatio;
-					            $(this).css({'margin-top' : 0, 'margin-left' : -(videoWidth - windowWidth) / 2 + 'px'});
-
-					            $(this).width(videoWidth).height(videoHeight);
-					        }
-
-					        $('.homepage-hero-module .video-container video').addClass('fadeIn animated');
-
-					    });
-					}
-					</script>
+					
 				</body>
 				</html>
