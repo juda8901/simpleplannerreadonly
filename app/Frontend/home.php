6d810e160f11494b0696aa7636688b4c681ace55
Colin O'Connell <cooc5321@colorado.edu>
2017-12-11 20:44:34 -0700

    used wrong variable name

--- app/Frontend/home.php
+++ app/Frontend/home.php
@@ -348,7 +348,7 @@ width: 100%;
 								
 								date_default_timezone_set('America/Denver');
 								$currentdate = date('m/d/Y', time());
-								$time = strtotime($startTime);
+								$time = strtotime($startDate);
 								$eventstart = date('m/d/Y',$time);
 								if($eventstart < $currentdate){
 									break;
