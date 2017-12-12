30171c622130a8329fc190f5ec86d7ac500a7724
Michael Nicolaou <mikenicolaou1994@gmail.com>
2017-12-12 13:32:12 -0700

    f

--- app/Frontend/nav_bar.php
+++ app/Frontend/nav_bar.php
@@ -1,10 +1,7 @@
 <div class="w3-bar w3-white w3-top" style =" top:0; left:0; z-index:1;">
   <a href="https://simpleplanner.herokuapp.com" class="w3-left"><img src="https://simpleplanner.herokuapp.com/Frontend/images/treeLogo.png" style="margin-top: 5px; margin-left: 5px; width: 30px; height: 30px;"/></a>
-<<<<<<< HEAD
   <a href="Frontend/login.php" class="w3-bar-item w3-right w3-button w3-hover-light-gray" style="height: 40px;" id="login">Log In</a>
-=======
   <a href="https://simpleplanner.herokuapp.com/Frontend/login.php" class="w3-bar-item w3-right w3-button w3-hover-ligth-gray" style="height: 40px;" id="login">Log In</a>
->>>>>>> e8e71fabec263dc8b1a254c3ce94649f9abe64c8
   <a href="https://simpleplanner.herokuapp.com/Backend/logout.php" class="w3-bar-item w3-right w3-button w3-hover-light-gray" style="height: 40px;" id="logout">Log Out</a>
   <a onclick="document.getElementById('sign_up').style.display='block'" class=" w3-bar-item w3-right w3-button w3-hover-light-gray" style="height: 40px;" id="reg">Sign Up</a>
   <a onclick="<?php if($valid){	echo "document.getElementById('create_group').style.display='block'";} else {	echo "alert('You must log in first');window.location = 'https://simpleplanner.herokuapp.com/Frontend/login.php';";} ?>" class="w3-bar-item w3-right w3-button w3-hover-red" style="color: #F64060; height: 40px;" id="group">Create a Group</a>
