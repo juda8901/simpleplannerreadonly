15c77edb57f66a5c96ad9846c982b7a34ef28adc
Michael Nicolaou <mikenicolaou1994@gmail.com>
2017-12-12 01:04:57 -0700

    dd

--- app/Frontend/home.php
+++ app/Frontend/home.php
@@ -123,7 +123,7 @@ if($valid) $id=$_SESSION['id'];
 
 	<!-- search bar -->
 	<div style="background-color: grey;">
-	<form id="search_bar" style="width: 100%; text-align: center;">
+	<form id="search_bar" style="width: 100%; text-align: center;" onsubmit="return false">
 		<input type="text" placeholder="  Search for an event..." name="search-criteria" id="search-criteria" style="width: 40%; border-radius: 30px; font-size: large; padding: 0; margin: 0;"/>
 		<button type="submit" class="w3-button w3-hover-blue-grey" style="width: 30px; height: 30px; padding: 0; margin:0px; margin-bottom: 10px;" id="search" value="search"><img src="https://simpleplanner.herokuapp.com/Frontend/images/searchIconRed.png" style="width: 30px; height: 30px;"><i class="fa fa-search" style="zoom: 1.75;padding: 0; margin: 0;"></i></button>
 	</form>
