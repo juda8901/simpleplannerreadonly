46c1d4df6fda4eabf92a607ea4afe55e5156fc92
juda8901 <juda8901@colorado.edu>
2017-12-11 17:26:24 -0700

    Still struggling

--- app/Frontend/nav_bar.php
+++ app/Frontend/nav_bar.php
@@ -1,7 +1,7 @@
 <div class="w3-bar w3-blue-grey w3-top">
   <a href="https://simpleplanner.herokuapp.com" class="w3-bar-item w3-left w3-button w3-hover-blue-grey"><img src="https://simpleplanner.herokuapp.com/Frontend/images/treeLogo.png" style="width: 20px; height: 20px;"><span id="nav_title" style="display: none;">  Simpleplanner</span></a>
   <a href="Frontend/login.php" class="w3-bar-item w3-right w3-button w3-hover-white" id="login">Log In</a>
-  <a href="<?php session_unset();session_destroy(); ?>" class="w3-bar-item w3-right w3-button w3-hover-white" id="logout">Log Out</a>
+  <a href="<?php //session_unset();session_destroy(); ?>" class="w3-bar-item w3-right w3-button w3-hover-white" id="logout">Log Out</a>
   <a onclick="document.getElementById('sign_up').style.display='block'" class=" w3-bar-item w3-right w3-button w3-hover-white" id="reg">Sign Up</a>
   <a onclick="<?php if($valid){	echo "document.getElementById('create_group').style.display='block'";} else {	echo "alert('You must log in first');window.location = 'https://simpleplanner.herokuapp.com/Frontend/login.php';";} ?>" class=" w3-bar-item w3-right w3-button w3-hover-red" style="color: red;" id="group">Create a Group</a>
 </div>
